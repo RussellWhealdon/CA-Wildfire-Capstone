ef2e1fd4f3a0af1f29377e6ee2a43f1a51450474
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-04-14 17:29:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,30 +20,27 @@ background-size: cover;
 </style>
 """
 
-def main():
-    # Set page config
-    st.set_page_config(page_title='Wildfire Damage Analysis', layout='wide')
+
+# Set page config
+st.set_page_config(page_title='Wildfire Damage Analysis', layout='wide')
     
-    # Introduction section
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+# Introduction section
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-    st.title("Wildfire Damage Analysis")
+st.title("Wildfire Damage Analysis")
     
-    st.write("IntroductionThis dashboard presents an analysis of the economic impacts of wildfires, developed in collaboration with Deloitte's sustainability arm. The project aims to understand and predict the financial damages caused by wildfires, leveraging data on various environmental and economic factors. The predictive modeling was done using an XGBoost regression model, enhanced with SHAP and LIME for interpretability.")
+st.write("IntroductionThis dashboard presents an analysis of the economic impacts of wildfires, developed in collaboration with Deloitte's sustainability arm. The project aims to understand and predict the financial damages caused by wildfires, leveraging data on various environmental and economic factors. The predictive modeling was done using an XGBoost regression model, enhanced with SHAP and LIME for interpretability.")
 
-    # Rest of the app here
-    # Display data, interactive widgets, visualizations, etc.
+# Rest of the app here
+# Display data, interactive widgets, visualizations, etc.
 
-    #Load in data
-    data = pd.read_excel('climateprojdata_final.xlsx')
+#Load in data
+data = pd.read_excel('climateprojdata_final.xlsx')
 
-    #Drop/rename columns (XGBoost doesn't accept special characters)
-    data['El Nino'] = data['El Nino'].replace({'El Nino': 1, 'La Nina': 0})
-    data = data.drop(columns=['Unnamed: 0', 'COUNTY', 'Total Fires', 'Large Fires', 'Total Acres Burned'])
-    data = data.rename(columns={'.25 acres or <':'.25 acres or less', '5000 acres or >':'5000 acres or more'})
+#Drop/rename columns (XGBoost doesn't accept special characters)
+data['El Nino'] = data['El Nino'].replace({'El Nino': 1, 'La Nina': 0})
+data = data.drop(columns=['Unnamed: 0', 'COUNTY', 'Total Fires', 'Large Fires', 'Total Acres Burned'])
+data = data.rename(columns={'.25 acres or <':'.25 acres or less', '5000 acres or >':'5000 acres or more'})
 
-    st.write(data)
+st.write(data)
 
-
-if __name__ == "__main__":
-    main()
