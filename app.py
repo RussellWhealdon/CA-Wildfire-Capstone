5b7f359860423e60f550632c697681b7fa52d354
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-04-14 16:42:18 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import xgboost as xgb
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from xgboost import XGBRegressor
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+from sklearn.metrics import mean_absolute_error
+import shap
+import lime
+import lime.lime_tabular
+
+#Load in data
+data = pd.read_excel('climateprojdata_final.xlsx')
+
+#Drop/rename columns (XGBoost doesn't accept special characters)
+data['El Nino'] = data['El Nino'].replace({'El Nino': 1, 'La Nina': 0})
+data = data.drop(columns=['Unnamed: 0', 'COUNTY', 'Total Fires', 'Large Fires', 'Total Acres Burned'])
+data = data.rename(columns={'.25 acres or <':'.25 acres or less', '5000 acres or >':'5000 acres or more'})
