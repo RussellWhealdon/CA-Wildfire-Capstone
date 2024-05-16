d2a87f0e8621c89aeb7f4d13c91783ee43161033
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-05-16 12:50:24 -0400

    Update appdisplay.py

--- appdisplay.py
+++ appdisplay.py
@@ -142,7 +142,12 @@ and transformations important as we began to build things out.
 col7, col8 = st.columns(2)
 with col7:
     st.subheader("Target Variable Transformation")
-    st.markdown("""Blah Blah""")
+    st.markdown("""
+    Although the XGBoost Regressor model does not assume that the data be normally distributed the variance of our target variable was causing unstable predictions that did 
+    not capture the true nature of the dataset. In order to account for this, we used log transformation to lessen the range of our target variables. A visual representation 
+    of this can be seen in the appendix. The log transformation takes the log form of each target variable before the model gets trained. Once we have our predictions for 
+    the model, we then transform the predictions back to their original scale to evaluate the model's performance. We saw more accurate and stable predictions for our models 
+    which we believe to be because of the change in distribution.""")
 
 with col8:
     st.image("images/TransDist..png")
